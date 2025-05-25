634e8f24da9e1853028e31f7bb657106b17a1950
Loup-Garou911XD <90267658+Loup-Garou911XD@users.noreply.github.com>
2025-05-25 18:30:02 +0530

    asd

--- gui_maker.py
+++ gui_maker.py
@@ -94,13 +94,19 @@ class SettingWindow(bui.Window):
 
     def _add_button(self):
         y = 260 - len(self._layout) * 50
+        data = {'type': 'button', 'label': 'Button', 'position': (40, y), 'size': (200, 40)}
         btn = bui.buttonwidget(
-            parent=self.builder_panel, label='Button', position=(40, y), size=(200, 40)
+            parent=self.builder_panel,
+            label=data['label'],
+            position=data['position'],
+            size=data['size'],
+            on_activate_call=lambda: self._edit_button_attributes(btn, data),
         )
-        self._layout.append(
-            {'type': 'button', 'label': 'Button', 'position': (40, y), 'size': (200, 40)}
-        )
+        self._layout.append(data)
         self._widgets.append(btn)
+
+    def _edit_button_attributes(self, btn_widget, data):
+        AttributeEditorWindow(self, btn_widget, data)
 
     def _add_label(self):
         y = 260 - len(self._layout) * 50
@@ -141,6 +147,126 @@ class SettingWindow(bui.Window):
             edit=self._root_widget,
             transition="out_right",
         )
+
+
+class AttributeEditorWindow(bui.Window):
+    def __init__(self, parent_window, btn_widget, data):
+        self._parent_window = parent_window
+        self._btn_widget = btn_widget
+        self._data = data
+        self.width = 320
+        self.height = 240
+        super().__init__(
+            root_widget=bui.containerwidget(size=(self.width, self.height), transition='in_scale')
+        )
+        bui.textwidget(
+            parent=self._root_widget,
+            position=(self.width * 0.5, self.height - 40),
+            size=(0, 0),
+            h_align='center',
+            v_align='center',
+            text='Edit Button Attributes',
+            scale=1.0,
+            color=(1, 1, 1),
+        )
+        # Label
+        bui.textwidget(
+            parent=self._root_widget,
+            position=(30, self.height - 80),
+            size=(0, 0),
+            text='Label:',
+            scale=0.8,
+            color=(1, 1, 1),
+            h_align='left',
+        )
+        self._label_field = bui.textwidget(
+            parent=self._root_widget,
+            editable=True,
+            position=(100, self.height - 85),
+            size=(180, 40),
+            text=self._data['label'],
+            v_align='center',
+            h_align='left',
+            color=(1, 1, 1),
+            maxwidth=180,
+        )
+        # X Position
+        bui.textwidget(
+            parent=self._root_widget,
+            position=(30, self.height - 120),
+            size=(0, 0),
+            text='X:',
+            scale=0.8,
+            color=(1, 1, 1),
+            h_align='left',
+        )
+        self._x_field = bui.textwidget(
+            parent=self._root_widget,
+            editable=True,
+            position=(100, self.height - 125),
+            size=(60, 40),
+            text=str(self._data['position'][0]),
+            v_align='center',
+            h_align='left',
+            color=(1, 1, 1),
+            maxwidth=60,
+        )
+        # Y Position
+        bui.textwidget(
+            parent=self._root_widget,
+            position=(170, self.height - 120),
+            size=(0, 0),
+            text='Y:',
+            scale=0.8,
+            color=(1, 1, 1),
+            h_align='left',
+        )
+        self._y_field = bui.textwidget(
+            parent=self._root_widget,
+            editable=True,
+            position=(200, self.height - 125),
+            size=(60, 40),
+            text=str(self._data['position'][1]),
+            v_align='center',
+            h_align='left',
+            color=(1, 1, 1),
+            maxwidth=60,
+        )
+        # Save button
+        bui.buttonwidget(
+            parent=self._root_widget,
+            label='Save',
+            position=(self.width * 0.5 - 60, 30),
+            size=(120, 40),
+            on_activate_call=self._save,
+        )
+        # Cancel button
+        bui.buttonwidget(
+            parent=self._root_widget,
+            label='Cancel',
+            position=(self.width * 0.5 - 60, 80),
+            size=(120, 40),
+            on_activate_call=self._close,
+        )
+
+    def _save(self):
+        # Get updated values
+        label = bui.textwidget(query=self._label_field)
+        try:
+            x = int(bui.textwidget(query=self._x_field))
+            y = int(bui.textwidget(query=self._y_field))
+        except Exception:
+            bui.screenmessage('Invalid position values!', color=(1, 0, 0))
+            return
+        # Update data
+        self._data['label'] = label
+        self._data['position'] = (x, y)
+        # Update the button widget
+        bui.buttonwidget(edit=self._btn_widget, label=label, position=(x, y))
+        self._close()
+
+    def _close(self):
+        bui.containerwidget(edit=self._root_widget, transition='out_scale')
 
 
 # ba_meta export plugin
